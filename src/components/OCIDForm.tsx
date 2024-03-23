4f050cd18f27617e226f1affdf4e1562249328b4
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-03-23 20:49:17 +0000

    enhance: sort by highest scoring matches first

--- src/components/OCIDForm.tsx
+++ src/components/OCIDForm.tsx
@@ -1,14 +1,14 @@
 import { testData } from "../testdata/getocids";
 import OCIDFormSection from "./OCIDFormSection";
+import "./OCIDForm.css";
 
 export const OCIDForm = () => {
+    const sortedData = testData.extra.results.sort((a, b) => a.results[0].dist - b.results[0].dist)
 
     return (
         <form>
             {
-                testData.extra.results.map((item) => {
-                    return <OCIDFormSection item={item}/>
-                })
+                sortedData.map((item) => <OCIDFormSection item={item}/>)
             }
         </form>
     )
