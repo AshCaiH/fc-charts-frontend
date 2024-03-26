03745b0feb041d76aa17c91659b5542802bc4c96
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-03-26 19:19:56 +0000

    enhance: display single user's total score over time

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -11,8 +11,15 @@ import {
     Legend,
 } from 'chart.js';
 
-const dates = chartData[0].data.map(item => new Date(item.date));
-const scores = chartData[0].data.map(item => item.fcAverage);
+
+const allReviews : {date: number, game: string, score: number, runningScore: number }[] = chartData.map(game => game.data.map(review => {
+    return {
+        date: Date.parse(review.date),
+        game: game.game,
+        score: review.fcAverage,
+        runningScore: 0,
+    }
+})).flat();
 
 ChartJS.register(
     CategoryScale,
@@ -24,9 +31,25 @@ ChartJS.register(
     Legend
 );
 
-export const options = {
+export const myData = chartData[1].data.map(item => {return {x: new Date(item.date), y: item.fcAverage}});
+
+const runningGameScore = {}
+chartData.map(item => runningGameScore[item.game] = 0);
+console.log(runningGameScore);
+
+allReviews.sort((a, b) => a.date - b.date);
+
+allReviews.forEach((item, index) => {
+    runningGameScore[item.game] = item.score;
+    allReviews[index]["runningScore"] = Object.values(runningGameScore).reduce((prev:number, now:number) => {return prev + now}, 0) 
+});
+
+console.log(allReviews);
+
+export const options:any = {
     responsive: true,
     maintainAspectRatio: false,
+    animations: false,
     plugins: {
         legend: {
             position: 'top' as const,
@@ -44,7 +67,6 @@ export const options = {
     }
 };
 
-export const myData = chartData[1].data.map(item => {return {x: new Date(item.date), y: item.fcAverage}});
 
 // const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
@@ -52,8 +74,8 @@ export const data = {
     // labels,
     datasets: [
         {
-            label: 'Dataset 1',
-            data: myData,
+            label: 'Username',
+            data: allReviews.map(item => {return {y: item.runningScore, x: new Date(item.date)}}),
             borderColor: 'rgb(255, 99, 132)',
             backgroundColor: 'rgba(255, 99, 132, 0.5)',
         },
